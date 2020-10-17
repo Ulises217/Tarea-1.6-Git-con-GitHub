10c07e0816e45590691e3109aa28a6219b4710ea
ULISES JARED LEMUS CANO <18311052170283@cbtis217.edu.mx>
2020-10-16 22:11:42 -0500

    subiendo el problema 1

--- problema1/codigo1.js
+++ problema1/codigo1.js
@@ -1,4 +1,14 @@
-var input=document.getElementById("radio").value;
+
 var boton=document.getElementById('boton');
-var radio=parseFloat(input);
-var respuesta=4.3*3.14159*radio*radio*radio;
+var div=document.getElementById('respuesta');
+
+function operacion()
+{
+
+    var input=document.getElementById("radio").value;
+    var radio=parseFloat(input);
+    var respuesta=((4.0/3)*3.14159*radio*radio*radio).toFixed(3);
+    div.innerHTML="VOLUMEN = "+String(respuesta);
+
+}
+boton.addEventListener("click", operacion);
